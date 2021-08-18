d8e79553e3de29cb564f0becff71acc9a29ab656
DoodlesEpic <you@example.com>
2021-08-18 18:01:09 -0300

    Listar PECs e PLs

--- democraz_web/views.py
+++ democraz_web/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+import requests
 
 
 def inicio(request):
-    return render(request, 'index.html')
+
+    url = 'https://dadosabertos.camara.leg.br/api/v2/proposicoes?itens=100&ordem=ASC&ordenarPor=id'
+    pecs = requests.get(url).json()
+
+    context = {
+        'pecs': pecs,
+    }
+    return render(request, 'index.html', context)
