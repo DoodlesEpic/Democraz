c84d0af2b8bb722e2f346940b78ec9a9c71c3133
DoodlesEpic <doodles33@protonmail.com>
2022-06-15 20:42:44 -0300

    Fix Heroku deployment static files problem

--- democraz_brasil/settings.py
+++ democraz_brasil/settings.py
@@ -16,6 +16,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Fix Heroku deployment
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
