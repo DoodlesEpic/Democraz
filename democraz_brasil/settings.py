30d855fb7b90f9d667c4e323628afddb5ba931fe
DoodlesEpic <doodles33@protonmail.com>
2022-06-15 20:56:10 -0300

    Attempt to fix another static error

--- democraz_brasil/settings.py
+++ democraz_brasil/settings.py
@@ -19,11 +19,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, "static"),
 )
 
 # Quick-start development settings - unsuitable for production
