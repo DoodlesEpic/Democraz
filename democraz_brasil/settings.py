9681c0271e639f61564c99fa6640d4ebb333d625
DoodlesEpic <doodles33@protonmail.com>
2022-06-15 21:43:38 -0300

    Fix unsupported operand str on database config

--- democraz_brasil/settings.py
+++ democraz_brasil/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'democraz_brasil.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
