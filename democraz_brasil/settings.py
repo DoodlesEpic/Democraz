d524cc84f1918bfaf24653f35f2d93afcf5a3130
Doodles <moraes.eduardo@proton.me>
2022-12-17 22:48:51 -0300

    Set up Render deploy with allowed hosts envvar

--- democraz_brasil/settings.py
+++ democraz_brasil/settings.py
@@ -30,8 +30,12 @@ SECRET_KEY = os.environ.get("DEMOCRAZ_DJANGO_SECRET", "rEWKYEXweRppHmUfGw59dWH36
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [ "democraz.herokuapp.com" ]
+# https://docs.djangoproject.com/en/3.0/ref/settings/#allowed-hosts
+ALLOWED_HOSTS = []
 
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:    
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
