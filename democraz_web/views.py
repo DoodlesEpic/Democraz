9ee024761a719f33cf633f5402241cff5c643df1
Doodles <moraes.eduardo@proton.me>
2023-02-07 07:32:48 -0300

    feat: Parallel http calls
    Previously http calls were done with the requests library which meant it
    was completelly synchronous and thus slow since we have to make around
    16 requests per page to source all the information we need. Now requests
    are done using aiohttp in an async manner, the view is also async.
    
    Hopefully these changes should bring a noticeable performance
    improvement.

--- democraz_web/views.py
+++ democraz_web/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
-import requests
+import aiohttp
 
-
-def inicio(request, pagina=1):
+async def inicio(request, pagina=1):
 
     url = f'https://dadosabertos.camara.leg.br/api/v2/proposicoes?itens=15&pagina={pagina}&ordem=ASC&ordenarPor=id'
-    pecs = requests.get(url).json()
 
-    for pec in pecs['dados']:
-        pec['proposicao'] = requests.get(pec['uri']).json()
-
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            pecs = await response.json()
+        for pec in pecs['dados']:
+            async with session.get(pec['uri']) as response: 
+                pec['proposicao'] = await response.json()
+        
     context = {'pecs': pecs, 'pagina': pagina}
 
     return render(request, 'index.html', context)
