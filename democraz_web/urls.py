671a9bde344804f893bfbc1919029be08d0852e1
Doodles <moraes.eduardo@proton.me>
2023-02-07 15:44:09 -0300

    feat: Details page routing

--- democraz_web/urls.py
+++ democraz_web/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.inicio, name='inicio'),
     path('<int:pagina>', views.inicio, name='inicio'),
+    path('detalhes/<int:pec>', views.detalhes, name='detalhes'),
 ]
