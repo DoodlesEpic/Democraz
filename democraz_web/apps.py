83482982102529b4b39f9ebb08395c3d95b132a7
Doodles <moraes.eduardo@proton.me>
2026-01-05 22:43:11 -0300

    chore: remove django project from tree

--- democraz_web/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class DemocrazAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'democraz_web'
--- democraz_web/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.inicio, name='inicio'),
-    path('<int:pagina>', views.inicio, name='inicio'),
-    path('detalhes/<int:pec>', views.detalhes, name='detalhes'),
-]
