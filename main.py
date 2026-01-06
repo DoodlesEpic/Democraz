1f03852c66df4d775e54fd38715b7df73f7e2192
Doodles <moraes.eduardo@proton.me>
2026-01-06 00:08:02 -0300

    feat: add pagination

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
 
 app = Flask(__name__)
@@ -7,12 +7,16 @@ api_url = "https://dadosabertos.camara.leg.br/api/v2"
 
 @app.route("/")
 def main():
+    page = request.args.get("pagina", 1)
+
     try:
-        request_url = api_url + "/proposicoes?itens=30&ordem=ASC&ordenarPor=id"
+        request_url = (
+            f"{api_url}/proposicoes?itens=30&ordem=ASC&ordenarPor=id&pagina={page}"
+        )
         response = requests.get(request_url)
         response.raise_for_status()
         data = response.json()
 
-        return render_template("index.html", data=data)
+        return render_template("index.html", data=data, current_page=int(page))
     except requests.exceptions.RequestException as e:
         return f"An error occurred: {e}", 500
