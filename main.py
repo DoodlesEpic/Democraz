0f5b9a7d393946b1d7bacacdb7e1ce8f4ad2129e
Doodles <moraes.eduardo@proton.me>
2026-01-05 23:56:10 -0300

    feat: base implementation and template

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
+api_url = "https://dadosabertos.camara.leg.br/api/v2"
 
 
 @app.route("/")
 def main():
-    return render_template("index.html")
+    try:
+        request_url = api_url + "/proposicoes?itens=30&ordem=ASC&ordenarPor=id"
+        response = requests.get(request_url)
+        response.raise_for_status()
+        data = response.json()
+
+        return render_template("index.html", data=data)
+    except requests.exceptions.RequestException as e:
+        return f"An error occurred: {e}", 500
